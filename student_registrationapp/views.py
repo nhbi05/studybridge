72c3731cfa05278acc3f45e7d38c242e07b38ff9
Housnah Nansereko <nhousnahishaq@gmail.com>
2024-12-22 00:04:45 +0300

    Updating models

--- student_registrationapp/views.py
+++ student_registrationapp/views.py
@@ -1,26 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import StudentRegistrationForm
 # Create your views here.
-
-
-
-def register(request):
-    if request.method == 'POST':
-        form = StudentRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('dashboard')  # Redirect to their personalized dashboard
-    else:
-        form = StudentRegistrationForm()
-    return render(request, 'accounts/register.html', {'form': form})
-
-def dashboard(request):
-    # This will show content based on user's course and year
-    user = request.user
-    context = {
-        'course': user.get_course_display(),
-        'year': user.get_year_of_study_display(),
-    }
-    return render(request, 'accounts/dashboard.html', context)
--- studys_app/urls.py
+++ studys_app/urls.py
@@ -1,6 +1,3 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('notes/', views.list_notes, name='list_notes'),
-]
