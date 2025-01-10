25f329cf988611da3577394905a363038b2f9b47
Housnah Nansereko <nhousnahishaq@gmail.com>
2025-01-10 23:57:42 +0300

    added the logout functionality

--- apps/student_authentication/urls.py
+++ apps/student_authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register_student, login_student
+from .views import register_student, login_student,logout_student
 
 urlpatterns = [
     path('register/', register_student, name='register'),
     path('login/', login_student, name='login'),
+    path('logout/', logout_student, name='login'),
 ]
